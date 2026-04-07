055fec4ccab2a58def5b23601cf77c64f30e57ad
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2026-04-07 20:40:48 +0700

    remove: next.js project

--- user-management-next/app/actions/auth/sign-out.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { deleteSession } from "@/lib/auth/session";
-import { redirect } from "next/navigation";
-
-export async function logoutAction() {
-  await deleteSession();
-  return redirect("/sign-in");
-}
--- user-management-next/schemas/auth/sign-in.schema.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-export const signInSchema = z.object({
-  email: z.email("Email tidak valid"),
-  password: z.string().min(6, "Password min 6 karakter"),
-});
-
-export type signInInput = z.infer<typeof signInSchema>;
